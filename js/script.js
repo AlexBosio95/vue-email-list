ea39fe914991407af5a0f03aa5051f6f354ad156
AlexBosio95 <alex.bosio1995@gmail.com>
2022-07-13 16:10:35 +0200

    feat: get randome email

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+const app = new Vue ({
+    el : '#app',
+    data: {
+        emailRandome: [],
+        isShow: false,
+    },
+
+    methods: {
+        getRandomeEmail: function(){
+
+            const numberEmail = 9
+
+            this.emailRandome = []
+
+            this.isShow = false
+            
+        
+            for (let i = 0; i <= numberEmail; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                const result = response.data.response
+                this.emailRandome.push(result)
+
+                if (numberEmail === (this.emailRandome.length + 1)) {
+                    this.isShow = true
+                }
+            });
+            }
+
+            console.log(this.emailRandome.length)
+
+            
+
+            
+            
+        }
+    },
+
+
+
+})
